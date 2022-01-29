f13c9f4ac06649d2d731429e32b75c880ca2b8a4
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2022-01-28 19:24:13 -0700

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
 import React from "react";
 
-import { Navbar, Sidebar } from "./components";
 import { Home } from "./pages";
 
-const App: React.FC = () => {
-  return (
-    <div className="grid grid-cols-12" style={{ height: "inherit" }}>
-      <div className="col-span-12 md:col-span-2">
-        <Navbar />
-      </div>
+const App: React.FC = () =>
+  <Home />
 
-      <div className="col-span-7">
-        {/* React Routing Here */}
-        <Home />
-      </div>
-
-      <div className="col-span-12 md:col-span-3">
-        <Sidebar />
-      </div>
-    </div>
-  );
-};
 
 export default App;
